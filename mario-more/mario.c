9584c140b05f1fb5b9b908fbf064c6015d0c399c
jaylogical070 <81265920+jaylogical070@users.noreply.github.com>
2022-06-15 06:25:21 +0000

    Wed, Jun 15, 2022, 6:25 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 
 int main(void)
+string hash = "#";
+string sp = " " ;
+
 {
 int height = get_int("input your height, from 1 to 8:  ");
  if(height>=1 || height<=8){
 //code
 
  do{
- 
+
  }while(height>=1 || height<=8);
 
 printf("input height within the height interval");
