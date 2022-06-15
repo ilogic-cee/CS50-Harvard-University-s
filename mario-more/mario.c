cc62b8c748fff7410043ec7788521ba847642d7c
jaylogical070 <81265920+jaylogical070@users.noreply.github.com>
2022-06-15 05:31:38 +0000

    Wed, Jun 15, 2022, 5:31 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -5,8 +5,13 @@ int main(void)
 {
 
 
-int height = get_integer("input your height, from 1 to 8");
+int height = get_int("input your height, from 1 to 8:  ");
+ if(height>=1 || height<=8){
+//code
 
+ } else
+
+ printf("input value within the interval")
 
 
 }
