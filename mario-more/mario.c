cbf4cc003f97e2f8f7ecbbfba4a6686c409e2035
jaylogical070 <81265920+jaylogical070@users.noreply.github.com>
2022-06-16 06:27:57 +0000

    Thu, Jun 16, 2022, 6:27 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int Trow, row, sp, h, hash;
 
 int main(void)
 {
+
+int Trow, row, sp, h, hash;
 
 do{
 Trow = get_int("input your height, from 1 to 8 :  \n");
@@ -19,7 +20,7 @@ for(row =1; row <= Trow; row++){
   for(hash=1; hash <= 2*row; hash++){
    printf("#");
    printF("\n");
-   
+
  }
 }while(Trow>=1 || Trow<=8);
 
