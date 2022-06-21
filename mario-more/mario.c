8825c6ab847eb1f4313ae79945b988640e91d54a
jaylogical070 <81265920+jaylogical070@users.noreply.github.com>
2022-06-21 06:06:25 +0000

    Tue, Jun 21, 2022, 6:06 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -9,7 +9,7 @@ int Trow, row, sp, h, hash;
 do{
   Trow = get_int("input your height, from 1 to 8 :  \n");
 }while(Trow<=1 || Trow>=8);
-
+  
 // print spaces
 for(row =1; row <= Trow; row++){
  for(sp =1; sp<=(Trow - row); sp++){
