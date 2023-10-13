9383ed631795792dbf07b98bcea542a7f8ed32ca
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-10-13 14:29:59 +0000

    Fri, Oct 13, 2023, 4:29 PM +02:00

--- hello.c
+++ hello.c
@@ -3,8 +3,8 @@
 
 int main (void)
 {
-string name = get_string("what is your name? "); 
-    printf("hello, world \n");
+string name = get_string("what is your name? ");
+    printf("hello %s \n", name);
 }
 
 
