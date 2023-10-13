95b9555f29c2d5202fe84c93f816fb22024eef58
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-10-13 16:25:11 +0000

    Fri, Oct 13, 2023, 6:25 PM +02:00

--- population/population.c
+++ population/population.c
@@ -9,13 +9,17 @@ int main(void)
 do
 {
 start = get_int("start size: ");
-
-
 }
 while(start < 9);
 
 
     // TODO: Prompt for end size
+
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < start);
 
     // TODO: Calculate number of years until we reach threshold
 
