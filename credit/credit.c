4768e9ad8e040d54ca5375669d612f20da4b827a
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-10-14 13:16:55 +0000

    Sat, Oct 14, 2023, 3:16 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,5 +3,19 @@
 
 int main(void)
 {
+    loing cardN
+    // prompt for users credit card
+cardN = get_Long_long("Enter your credit card Number: ");
+
+if ()
+{}
+else
+{}
+}
+
+bool is_Valid_credit__card(long long cardN)
+{
+ int sum = 0 ;
+ int digit_count = 0 ; 
 
 }
