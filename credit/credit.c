042c8821ed8b338c77c7a16eed9863e1f7f15d00
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-10-14 13:59:22 +0000

    Sat, Oct 14, 2023, 3:59 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,19 +1,28 @@
  #include <cs50.h>
 #include <stdio.h>
 
+
+// calling the function is_valid_card
+
+bool is_Valid_card(long long cardN);
 int main(void)
 {
     long long cardN;
     // prompt for users credit card
-cardN = get_Long_long("Enter your credit card Number: ");
+cardN = get_Long_long("Enter your credit card Number fully : ");
 
-if ()
-{}
+// checking if the card is valid or not based on the is valid function
+if (is_Valid_card(cardN))
+{
+    printf("Valid\n");
+}
 else
-{}
+{
+    printf("invalid\n");
+}
 }
 
-bool is_Valid__card(long long cardN)
+bool is_Valid_card(long long cardN)
 {
 
  int Total = 0 ;
@@ -21,13 +30,22 @@ bool is_Valid__card(long long cardN)
 
  while (cardN > 0 )
  {
-    // getting the rightmost digit of the card Number 
+    // getting the rightmost digit of the card Number
     int digit = cardN%10;
  }
 
   if (count %2 == 1)
    {
     digit *= 2;
+    Total += digit %10 + digit/10;
 
    }
+   else
+   {
+    Total +=digit ;
+   }
+   cardN /=10;
+   count ++;
 }
+
+return Total % 10 == 0;
