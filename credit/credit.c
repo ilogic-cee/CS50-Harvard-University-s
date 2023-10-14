efd5b10412d52ceccf45ee1f778056dbbfa1360a
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-10-14 14:33:43 +0000

    Sat, Oct 14, 2023, 4:33 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,52 +1,44 @@
- #include <cs50.h>
+#include <cs50.h>
 #include <stdio.h>
 
-
 // calling the function is_valid_card
-
 bool is_valid_card(long long cardN);
 
-int main(void)
-{
-    long long cardN;
-    // prompt for users credit card
- cardN = get_long_long("Enter your credit card Number fully : ");
+int main(void) {
+  long long cardN;
+  // prompt for users credit card
+  cardN = get_long_long("Enter your credit card Number fully : ");
 
-// checking if the card is valid or not based on the is valid function
-if (is_valid_card(cardN))
-{
+  // checking if the card is valid or not based on the is valid function
+  if (is_valid_card(cardN)) {
     printf("VALID\n");
-}
-else
-{
+  } else {
     printf("INVALID\n");
+  }
 }
-}
 
-bool is_alid_card(long long cardN)
-{
+bool is_valid_card(long long cardN) {
+  int total = 0;
+  int count = 0;
 
- int Total = 0 ;
- int count = 0 ;
-
- while (cardN > 0 )
- {
+  while (cardN > 0) {
     // getting the rightmost digit of the card Number
-    int digit = cardN%10;
- }
+    int digit = cardN % 10;
 
-  if (count %2 == 1)
-   {
- int digit *= 2;
-    Total += digit %10 + digit/10;
+    // if the count is odd, double the digit
+    if (count % 2 == 1) {
+      digit *= 2;
+    }
 
-   }
-   else
-   {
-    Total +=digit ;
-   }
-   cardN /=10;
-   count ++;
+    // add the digit to the total, even if it is double-digits
+    total += digit;
+
+    // remove the rightmost digit from the card number
+    cardN /= 10;
+
+    // increment the count
+    count++;
+  }
+
+  return total % 10 == 0;
 }
-
-return Total % 10 == 0;
