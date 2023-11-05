b70da227b478e18db452d2d60bebbcd18d61e8b8
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-05 08:42:52 +0000

    Sun, Nov 5, 2023, 10:42 AM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,17 +6,8 @@
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string word)
-{
-int score = 0 ; // to keep track of the score
-for (int i = 0; i < strlen(word) ; i++ )
-if (isalpha(word[i]))
-{
-    score += POINTS[tolower(word[i])- 'a'];
-}
-
-return score ;
-}
+//calling the computer score function
+int compute_score(string word);
 
 
 int main(void)
@@ -30,14 +21,33 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+// TODO: Print the winner
+
+if (score1 > score2) {
+  printf("Player 1 wins!\n");
+} else
+
+if (score2 > score1) {
+  printf("Player 2 wins!\n");
+} else {
+  printf("Tie!\n");
 }
 
+    return 0;
+}
+
+// computing the score for a word function implementation
 int compute_score(string word)
+// TODO: Compute and return score for string
 {
+    int score = 0;
+    for (int i = 0; i < strlen(word); i++)
+    {
+if (isalpha(word[i]))
+{
+    score += POINTS[tolower(word[i]) - 'a'];
+}
+    }
     // TODO: Compute and return score for string
-
-    // array of characters
-
-    sum = sum + 1 ;
-
+    return score ;
 }
