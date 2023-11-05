b65123f71882ffee86355812d1cb123938d823d2
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-05 12:42:59 +0000

    Sun, Nov 5, 2023, 2:42 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,8 @@ int main(int argc, string argv[])
 bool vote(string name) {
   // Check if the name matches a valid candidate
   for (int i = 0; i < candidate_count; i++) {
-    if (strcmp(candidates[i].name.c_str(), name.c_str()) == 0) {
+    if (strcmp(candidates[i].name, name) == 0) {
+
       // Update the vote count for the matching candidate
       candidates[i].votes++;
       return true;
