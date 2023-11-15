c253a233abc4e331f611285544eed677c0824a40
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-15 19:08:08 +0000

    Wed, Nov 15, 2023, 9:08 PM +02:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,12 +1,12 @@
 def main():
-    # Prompt the user for mass
+    # Prompt the user for mass in kilograms
     mass = int(input("Enter mass in kilograms: "))
 
-    # Convert mass to energy
-    energy = mass * 299792458**2
+    # Compute the equivalent number of Joules
+    joules = mass * (299792458 ** 2)
 
-    # Print the energy
-    print(f"Energy in Joules: {energy}")
+    # Print the equivalent number of Joules
+    print(f"Equivalent energy in Joules: {joules}")
 
 if __name__ == "__main__":
     main()
