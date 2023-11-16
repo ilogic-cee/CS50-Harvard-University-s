968e54b14e711faf1df586d3c5953af8e3671ebf
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-16 07:10:19 +0000

    Thu, Nov 16, 2023, 9:10 AM +02:00

--- smiley/colorize.c
+++ smiley/colorize.c
@@ -1,6 +1,6 @@
-// colorize.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "bmp.h"
 
 void colorize(int height, int width, RGBTRIPLE image[height][width]);
@@ -48,45 +48,12 @@ int main(int argc, char *argv[])
         return 4;
     }
 
-    int height = abs(bi.biHeight);
+    int height = labs(bi.biHeight); // Use labs for absolute value
     int width = bi.biWidth;
-
-    // Allocate memory for image
-    RGBTRIPLE (*image)[width] = calloc(height, sizeof(RGBTRIPLE[width]));
-    if (image == NULL)
-    {
-        fprintf(stderr, "Not enough memory to store image.\n");
-        fclose(outptr);
-        fclose(inptr);
-        return 5;
-    }
 
     // ... (rest of your main function logic)
 
+    fclose(inptr);
+    fclose(outptr);
     return 0;
-}
-
-// ... (unchanged code)
-
-void colorize(int height, int width, RGBTRIPLE image[height][width])
-{
-    // Define the color you want to use for non-black pixels
-    RGBTRIPLE newColor;
-    newColor.rgbtRed = 255;    // Red component
-    newColor.rgbtGreen = 0;    // Green component
-    newColor.rgbtBlue = 0;     // Blue component
-
-    // Iterate through each pixel in the image
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            // Check if the pixel is black (assuming RGB values 0, 0, 0)
-            if (image[i][j].rgbtBlue == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtRed == 0)
-            {
-                // Change the color of black pixels
-                image[i][j] = newColor;
-            }
-        }
-    }
 }
