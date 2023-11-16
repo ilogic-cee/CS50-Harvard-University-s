31f4b5a5df8fcedaa9441a06a4c5f3e12024bce0
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-16 06:13:53 +0000

    Thu, Nov 16, 2023, 8:13 AM +02:00

--- smiley/colorize.c
+++ smiley/colorize.c
@@ -1,85 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "bmp.h"
+// ... (previous code)
 
-void colorize(int height, int width, RGBTRIPLE image[height][width]);
-
-int main(int argc, char *argv[])
+void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Check command-line arguments
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s infile outfile\n", argv[0]);
-        return 1;
-    }
+    // Declare padding
+    int padding = (4 - (width * sizeof(RGBTRIPLE)) % 4) % 4;
 
-    // Open input file
-    FILE *infile = fopen(argv[1], "rb");
-    if (infile == NULL)
-    {
-        fprintf(stderr, "Could not open %s.\n", argv[1]);
-        return 2;
-    }
+    // Define the color you want to use for non-black pixels
+    RGBTRIPLE newColor;
+    newColor.rgbtRed = 255;    // Red component
+    newColor.rgbtGreen = 0;    // Green component
+    newColor.rgbtBlue = 0;     // Blue component
 
-    // Open output file
-    FILE *outfile = fopen(argv[2], "wb");
-    if (outfile == NULL)
-    {
-        fclose(infile);
-        fprintf(stderr, "Could not create %s.\n", argv[2]);
-        return 3;
-    }
-
-    // Read BMP headers
-    BITMAPFILEHEADER bf;
-    fread(&bf, sizeof(BITMAPFILEHEADER), 1, infile);
-
-    BITMAPINFOHEADER bi;
-    fread(&bi, sizeof(BITMAPINFOHEADER), 1, infile);
-
-    // Allocate memory for image
-    int height = abs(bi.biHeight);
-    int width = bi.biWidth;
-    RGBTRIPLE (*image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
-    if (image == NULL)
-    {
-        fclose(outfile);
-        fclose(infile);
-        fprintf(stderr, "Not enough memory to store image.\n");
-        return 4;
-    }
-
-    // Read pixels from input file
+    // Iterate through each pixel in the image
     for (int i = 0; i < height; i++)
     {
-        fread(image[i], sizeof(RGBTRIPLE), width, infile);
-        fseek(infile, padding, SEEK_CUR);
-    }
-
-    // Call colorize function
-    colorize(height, width, image);
-
-    // Write BMP headers to output file
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outfile);
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outfile);
-
-    // Write modified pixels to output file
-    for (int i = 0; i < height; i++)
-    {
-        fwrite(image[i], sizeof(RGBTRIPLE), width, outfile);
-
-        for (int k = 0; k < padding; k++)
+        for (int j = 0; j < width; j++)
         {
-            fputc(0x00, outfile);
+            // Check if the pixel is black (assuming RGB values 0, 0, 0)
+            if (image[i][j].rgbtBlue == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtRed == 0)
+            {
+                // Change the color of black pixels
+                image[i][j] = newColor;
+            }
         }
     }
-
-    // Free allocated memory
-    free(image);
-
-    // Close files
-    fclose(infile);
-    fclose(outfile);
-
-    return 0;
 }
+
+// ... (rest of the code)
