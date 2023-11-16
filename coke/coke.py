23300e7130d81bacba8e87f22f5b7c5fcf717329
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-16 21:53:34 +0000

    Thu, Nov 16, 2023, 11:53 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,25 +1,30 @@
-def main():
-    # Initialize variables
-    coke_price = 50
-    cents_inserted = 0
+# Initialize variables
+amount_due = 0
 
-    # Prompt the user for coins until the price is met or exceeded
-    while cents_inserted < coke_price:
-        # Prompt the user for a coin denomination
-        coin_denomination = int(input("Insert Coin: "))
+# Prompt the user to insert a coin until the amount due is at least 50 cents
+while amount_due < 50:
+    # Get user input for the coin
+    coin = int(input("Insert Coin: "))
 
-        # Check if the coin denomination is accepted
-        if coin_denomination in [25, 10, 5]:
-            cents_inserted += coin_denomination
-            print(f"Amount Due: {cents_inserted}")
-        else:
-            print("Invalid coin denomination. Please try again.")
+    # Check if the coin is a valid denomination
+    if coin == 25:
+        amount_due += 25
+    elif coin == 10:
+        amount_due += 10
+    elif coin == 5:
+        amount_due += 5
+    else:
+        # Print an error message for invalid coins
+        print("Invalid Coin. Accepted denominations: 25, 10, 5 cents.")
+        continue
 
-    # Calculate the change owed
-    change_owed = coke_price - cents_inserted
+    # Print the updated amount
+    print(f"Amount Due: {amount_due}")
 
-    # Print the change owed
+# Check if the user paid exactly 50 cents
+if amount_due == 50:
+    print("Thank you for your purchase!")
+else:
+    # Calculate and print the change owed
+    change_owed = max(0, amount_due - 50)
     print(f"Change Owed: {change_owed}")
-
-if __name__ == "__main__":
-    main()
