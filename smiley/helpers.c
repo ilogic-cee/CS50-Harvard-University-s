6675f8e7f305e56226172004f12538d8f5e0bfc6
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-16 06:10:16 +0000

    Thu, Nov 16, 2023, 8:10 AM +02:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -1,47 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <cs50.h>  // Include cs50.h for user input functions
-#include "bmp.h"
-
-void colorize(int height, int width, RGBTRIPLE image[height][width]);
-
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s infile outfile\n", argv[0]);
-        return 1;
-    }
-
-    // Open input file
-    FILE *infile = fopen(argv[1], "r");
-    if (infile == NULL)
-    {
-        fprintf(stderr, "Could not open %s.\n", argv[1]);
-        return 2;
-    }
-
-    // Open output file
-    FILE *outfile = fopen(argv[2], "w");
-    if (outfile == NULL)
-    {
-        fclose(infile);
-        fprintf(stderr, "Could not create %s.\n", argv[2]);
-        return 3;
-    }
-
-    // ... (rest of your main function logic)
-
-    return 0;
-}
+#include "helpers.h"
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Define the color you want to use for non-black pixels
     RGBTRIPLE newColor;
     newColor.rgbtRed = 255;    // Red component
-    newColor.rgbtGreen = 255;  // Green component
-    newColor.rgbtBlue = 255;   // Blue component
+    newColor.rgbtGreen = 0;    // Green component
+    newColor.rgbtBlue = 0;     // Blue component
 
     // Iterate through each pixel in the image
     for (int i = 0; i < height; i++)
