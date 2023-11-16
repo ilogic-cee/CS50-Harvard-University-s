d5b259a9859de713cde5d375fcb429b3b0e60fa1
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-16 20:17:28 +0000

    Thu, Nov 16, 2023, 10:17 PM +02:00

---
+++ coke/coke.py
@@ -0,0 +1,21 @@
+# Initialize variables
+amount_due = 0
+
+# Prompt the user to insert a coin until the amount due is at least 50 cents
+while amount_due < 50:
+    # Get user input for the coin
+    coin = int(input("Insert Coin: "))
+
+    # Check if the coin is a valid denomination
+    if coin in [25, 10, 5]:
+        # Update the amount due
+        amount_due += coin
+
+        # Print the updated amount due
+        print(f"Amount Due: {amount_due}")
+    else:
+        print("Invalid Coin. Accepted denominations: 25, 10, 5 cents.")
+
+# Calculate and print the change owed
+change_owed = amount_due - 50
+print(f"Change Owed: {change_owed}")
