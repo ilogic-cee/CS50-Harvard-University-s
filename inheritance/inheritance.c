3ad3aa2bd163b7ba62b16c7a84eddc50abba3d84
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-16 08:10:22 +0000

    Thu, Nov 16, 2023, 10:10 AM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -2,11 +2,86 @@
 #include <stdlib.h>
 #include <time.h>
 
-// ... (rest of the code remains unchanged)
+// Enum for blood types
+typedef enum
+{
+    A,
+    B,
+    O
+} Allele;
 
-// Function to print the family tree
+// Struct to represent a person
+typedef struct person
+{
+    Allele alleles[2];           // Blood type alleles
+    struct person *parents[2];   // Pointers to parents
+} Person;
+
+// Function prototypes
+Person *create_family(int generations);
+void free_family(Person *p);
+void print_family(const Person *p);
+
+// Function to generate a random allele
+Allele random_allele(void)
+{
+    int r = rand() % 3;
+    switch (r)
+    {
+    case 0:
+        return A;
+    case 1:
+        return B;
+    default:
+        return O;
+    }
+}
+
+// TODO: Implement create_family function
+Person *create_family(int generations)
+{
+    // Allocate memory for a new person
+    Person *p = malloc(sizeof(Person));
+
+    // Base case: no more generations
+    if (generations == 1)
+    {
+        p->parents[0] = NULL;
+        p->parents[1] = NULL;
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
+    }
+    else
+    {
+        // Recursive case: create parents and assign alleles
+        p->parents[0] = create_family(generations - 1);
+        p->parents[1] = create_family(generations - 1);
+        p->alleles[0] = random_allele();
+        p->alleles[1] = random_allele();
+    }
+
+    return p;
+}
+
+// TODO: Implement free_family function
+void free_family(Person *p)
+{
+    // Base case: person is NULL
+    if (p == NULL)
+    {
+        return;
+    }
+
+    // Recursive case: free parents and then free the person
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
+    free(p);
+}
+
+// TODO: Implement print_family function
 void print_family(const Person *p)
 {
+    // Your code for printing the family tree goes here
     if (p != NULL)
     {
         // Print the child's blood type
@@ -113,4 +188,19 @@ void print_family(const Person *p)
     }
 }
 
-// ... (rest of the code remains unchanged)
+int main(void)
+{
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Create a family with 3 generations
+    Person *family = create_family(3);
+
+    // Print the family tree
+    print_family(family);
+
+    // Free the allocated memory
+    free_family(family);
+
+    return 0;
+}
