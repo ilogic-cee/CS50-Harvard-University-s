ad7b939d6eadd95095e7d7bcddcee81639665da4
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-16 06:58:22 +0000

    Thu, Nov 16, 2023, 8:58 AM +02:00

--- smiley/colorize.c
+++ smiley/colorize.c
@@ -2,81 +2,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "bmp.h"
-#include "helpers.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width]);
 
 int main(int argc, char *argv[])
 {
-    // Check for correct number of command-line arguments
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s infile outfile\n", argv[0]);
-        return 1;
-    }
+    // ... (unchanged code)
 
-    // Open input file
-    FILE *infile = fopen(argv[1], "r");
-    if (infile == NULL)
-    {
-        fprintf(stderr, "Could not open %s.\n", argv[1]);
-        return 2;
-    }
-
-    // Open output file
-    FILE *outfile = fopen(argv[2], "w");
-    if (outfile == NULL)
-    {
-        fclose(infile);
-        fprintf(stderr, "Could not create %s.\n", argv[2]);
-        return 3;
-    }
-
-    // Read the BMP file headers (you may need to adjust this based on your specific headers)
+    // read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
+    fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+
+    // read infile's BITMAPINFOHEADER
     BITMAPINFOHEADER bi;
+    fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
-    fread(&bf, sizeof(BITMAPFILEHEADER), 1, infile);
-    fread(&bi, sizeof(BITMAPINFOHEADER), 1, infile);
-
-    // Create a 2D array to store the image pixels
-    RGBTRIPLE(*image)[bi.biWidth] = calloc(abs(bi.biHeight), sizeof(RGBTRIPLE[bi.biWidth]));
-    if (image == NULL)
+    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+        bi.biBitCount != 24 || bi.biCompression != 0)
     {
-        fclose(infile);
-        fclose(outfile);
-        fprintf(stderr, "Not enough memory to store image.\n");
-        return 4;
+        fclose(outptr);
+        fclose(inptr);
+        fprintf(stderr, "Unsupported file format.\n");
+        return 6;
     }
 
-    // Read the image pixels
-    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
-    {
-        fread(image[i], sizeof(RGBTRIPLE), bi.biWidth, infile);
-        fseek(infile, bi.biWidth * sizeof(RGBTRIPLE), SEEK_CUR);
-    }
+    int height = abs(bi.biHeight);  // Use abs for absolute value
+    int width = bi.biWidth;
 
-    // Call the colorize function to change pixel colors
-    colorize(abs(bi.biHeight), bi.biWidth, image);
+    // allocate memory for image
+    RGBTRIPLE (*image)[width] = calloc(abs(bi.biHeight), sizeof(RGBTRIPLE[bi.biWidth]));
 
-    // Write the modified headers to the output file
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outfile);
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outfile);
-
-    // Write the modified image pixels to the output file
-    for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
-    {
-        fwrite(image[i], sizeof(RGBTRIPLE), bi.biWidth, outfile);
-
-        // Add padding if necessary (you may need to adjust this based on your specific headers)
-        for (int k = 0; k < (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4; k++)
-        {
-            fputc(0x00, outfile);
-        }
-    }
-
-    // Close files and free memory
-    fclose(infile);
-    fclose(outfile);
-    free(image);
+    // ... (rest of your main function logic)
 
     return 0;
+}
+
+// ... (unchanged code)
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Define the color you want to use for non-black pixels
+    RGBTRIPLE newColor;
+    newColor.rgbtRed = 255;    // Red component
+    newColor.rgbtGreen = 0;    // Green component
+    newColor.rgbtBlue = 0;     // Blue component
+
+    // Iterate through each pixel in the image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Check if the pixel is black (assuming RGB values 0, 0, 0)
+            if (image[i][j].rgbtBlue == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtRed == 0)
+            {
+                // Change the color of black pixels
+                image[i][j] = newColor;
+            }
+        }
+    }
 }
