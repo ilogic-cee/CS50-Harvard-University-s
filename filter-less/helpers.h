f353eecbe37ff2bd9839e87087f4c966995803f5
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-16 22:35:11 +0000

    Fri, Nov 17, 2023, 12:35 AM +02:00

--- filter-less/helpers.h
+++ filter-less/helpers.h
@@ -5,15 +5,17 @@
 #include <math.h>
 
 // Function prototypes
-RGBTRIPLE get_clamped_sepia_pixel(RGBTRIPLE original_pixel);
+// Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
-// Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width]);
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width]);
+
+// Detect edges in image
+void edges(int height, int width, RGBTRIPLE image[height][width]);
+
 
 #endif // HELPERS_H
