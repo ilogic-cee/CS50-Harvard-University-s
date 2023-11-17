af929fb52aa29f78cce4923c317ba9eb6b5dba91
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-17 19:42:45 +0000

    Fri, Nov 17, 2023, 9:42 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -55,9 +55,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             float originalBlue = image[i][j].rgbtBlue;
 
 
-            int sepiaRed = cap(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
-            int sepiaGreen = cap(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
-            int sepiaBlue = cap(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+            int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            int sepiaGreen = round(0.349 * originalRed + 0.686 * originalGreen + 0.168 * originalBlue);
+            int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
 
             // Update pixel values with sepia values
             image[i][j].rgbtRed = sepiaRed;
