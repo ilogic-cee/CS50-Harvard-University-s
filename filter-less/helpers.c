18fa8bb2b9a72e44a7659860677f09c2d1b01335
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-17 15:08:03 +0000

    Fri, Nov 17, 2023, 5:08 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,7 +3,16 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE gray = (BYTE)(0.299 * image[i][j].rgbtRed + 0.587 * image[i][j].rgbtGreen + 0.114 * image[i][j].rgbtBlue);
+            image[i][j].rgbtRed = gray;
+            image[i][j].rgbtGreen = gray;
+            image[i][j].rgbtBlue = gray;
+        }
+    }
 }
 
 // Convert image to sepia
