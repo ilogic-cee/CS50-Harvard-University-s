e4ed1a4977f86da528f508f1e3f96ec5c3470ec6
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-18 17:40:41 +0000

    Sat, Nov 18, 2023, 7:40 PM +02:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,27 +1,33 @@
-item = input("Item: ")
-print("Calories: ", end = "")
+# Create dictionary with all fruits and its calories
 fruits = {
-"Apple" : "130",
-"Avocado" : "50",
-"Banana" : "110",
-"Cantaloupe" : "50",
-"Grapefruit" : "60",
-"Honeydew Melon" : "50",
-"Kiwifruit" : "90",
-"Lemon" : "15",
-"Lime" : "20",
-"Nectarine" : "60",
-"Orange" : "80",
-"Peach" : "60",
-"Pear" : "100",
-"Pineapple" : "50",
-"Plums" : "70",
-"Strawberries" : "50",
-"Sweet Cherries" : "100",
-"Tangerine" : "50",
-"Watermelon" : "80"
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80,
 }
 
-for fruit in fruits:
-if fruit == item:
-    print(fruits[fruit])
+# Get user input
+item = input("Item: ")
+
+# Loop through fruits dictionary
+for key in fruits:
+    # Find the fruit asked (remember to user lowercase)
+    if key == item.lower():
+        # Print fruit's calories
+        print("Calories:", fruits[key])
