4043e74b56d640950ad1d1741b30daaf6ffa5b91
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-18 15:47:35 +0000

    Sat, Nov 18, 2023, 5:47 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,18 +1,21 @@
 amount_due = 50
 cents = 0
 
-while cents < amount_due:
+while True:
     coin = int(input("Insert Coin: "))
 
     if coin not in [5, 10, 25]:
         print("Sorry, that coin is not accepted.")
-        continue  # Skip updating cents and amount_due
+        continue
 
-    cents -= coin  # Correct the subtraction
-    amount_due -= coin  # Update amount_due after accepting a valid coin
+    cents -= coin # Correct the subtraction
+    amount_due -= coin
+
+    if cents >= amount_due:
+        print("Enjoy your Coke!")
+        break
+
     print(f"Amount Due: {amount_due}")
 
-if cents == amount_due:
-    print("Enjoy your Coke!")
-else:
+if cents < amount_due:
     print(f"Change Owed: {amount_due - cents}")
