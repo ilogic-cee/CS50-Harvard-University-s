0215c06e59ee9e22b6a086c872cc33ce7eaeed58
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-18 17:10:38 +0000

    Sat, Nov 18, 2023, 7:10 PM +02:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -4,19 +4,23 @@ def main():
         "apple": 130,
         "avocado": 50,
         "sweet cherries": 100,
+        "kiwifruit": 90,
+        "pear": 100,
         # Add more fruits and their calorie information as needed
     }
 
     # Prompt the user to input a fruit
     fruit_input = input("Enter a fruit: ").lower()
 
+    # Get the calorie value for the input fruit, defaulting to None if not found
+    calorie_value = fruit_calories.get(fruit_input, None)
+
     # Check if the input is a valid fruit
-    if fruit_input in fruit_calories:
+    if calorie_value is not None:
         # Output the number of calories in one portion of that fruit
-        print(f"Calories: {fruit_calories[fruit_input]}")
+        print(f"Calories: {calorie_value}")
     else:
         # Ignore input that isn't a fruit
-        print("Invalid fruit")
 
 if __name__ == "__main__":
     main()
