992a1f00ed55b2dd4c731c3f34125be6e1f47318
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-19 19:24:41 +0000

    Sun, Nov 19, 2023, 9:24 PM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,9 +16,22 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    filename = sys.argv[1]
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            team = row
+            team["rating"] = int(team["rating"])
+            teams.append(team)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    for simulation in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -50,6 +63,13 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    rounds = len(teams)
+    if rounds >= 2:
+        teams = simulate_round(teams)
+        return simulate_tournament(teams)
+    else:
+        winner = teams[0]["team"]
+        return winner
 
 
 if __name__ == "__main__":
