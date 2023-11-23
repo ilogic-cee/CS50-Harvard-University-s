0dba4fa8eceebfb0f68534f5565b4ffa5af807ec
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-23 20:55:36 +0000

    Thu, Nov 23, 2023, 10:55 PM +02:00

--- movies/11.sql
+++ movies/11.sql
@@ -3,5 +3,5 @@ JOIN stars s ON m.id = s.movie_id
 JOIN people p ON s.person_id = p.id
 JOIN ratings r ON m.id = r.movie_id
 WHERE p.name = 'Chadwick Boseman'
-ORDER BY r.rating ASC
+ORDER BY r.rating DESC
 LIMIT 5;
