d85d207f8b92e530f98cf551237dfccb1a300f87
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-23 21:59:55 +0000

    Thu, Nov 23, 2023, 11:59 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -15,4 +15,9 @@ WHERE bsl.year = 2021 AND bsl.month = 7 AND bsl.day = 28 AND bsl.hour = 10 AND b
 SELECT * FROM atm_transactions
 WHERE atm_location = 'Leggett Street'
 AND year = 2021 AND month = 7 AND day = 28;
-
+--add name of withdraws from atm--
+SELECT a.*, p.name
+FROM atm_transactions a
+JOIN bank_accounts b ON a.account_number = b.account_number
+JOIN people p ON b.person_id = p.id
+WHERE a.atm_location 
