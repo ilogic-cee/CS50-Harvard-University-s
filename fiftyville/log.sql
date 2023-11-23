3792f8bce48ed84161b74ab57e650dabb280c159
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-23 22:14:56 +0000

    Fri, Nov 24, 2023, 12:14 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -24,4 +24,9 @@ WHERE a.atm_location = 'Leggett Street' AND a.year = 2021 AND a.month = 7 AND a.
 --witness 3 phone call investigation--
 SELECT *
 FROM phone_calls
-WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60; 
+WHERE year = 2021 AND month = 7 AND day = 28 AND duration < 60;
+--add names to calll list of callers--
+SELECT p.name, pc.caller, pc.receiver, pc.year, pc.month, pc.day, pc.duration
+FROM phone_calls pc
+JOIN people p ON pc.caller = p.phone_number
+WHERE pc.year = 2021 AND pc.month = 7 AND pc.day = 28 AND pc.duration < 60; 
