362f7b2cac4a2ec36d06a74e0e9f65d8564aa107
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-23 22:04:11 +0000

    Fri, Nov 24, 2023, 12:04 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -21,4 +21,3 @@ FROM atm_transactions a
 JOIN bank_accounts b ON a.account_number = b.account_number
 JOIN people p ON b.person_id = p.id
 WHERE a.atm_location = 'Loggett Street' AND a.year = 2021 AND a.month = 7 AND a.day = 28 AND a.transaction_type = 'withdraw';
-
