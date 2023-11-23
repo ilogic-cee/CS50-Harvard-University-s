0801978882efdb40f3debc4b082b2f6fb75454b7
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-23 06:57:54 +0000

    Thu, Nov 23, 2023, 8:57 AM +02:00

--- movies/10.sql
+++ movies/10.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT p.name FROM people p
 JOIN directors d ON p.id = d.person_id
 JOIN movies m ON d.movie_id = m.id
 JOIN ratings r ON m.id = r.movie_id
-WHERE r.ratings >= 9.0;
+WHERE r.rating >= 9.0;
