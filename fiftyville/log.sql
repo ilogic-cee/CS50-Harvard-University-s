4f9ee7269d5f75704e00a6087ff75d4b29a4b43a
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-23 21:22:45 +0000

    Thu, Nov 23, 2023, 11:22 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,3 +6,5 @@ SELECT * FROM interviews
 WHERE transcript LIKE '%bakery%';
 --witness 1 ruth--
 SELECT * FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10 AND minute BETWEEN 15 AND 25;
+--check against license plates--
+SELECT p.name, bsl.activity, bsl.license_plate, bsl.year, bsl.month, bsl.hour, bsl.minute
