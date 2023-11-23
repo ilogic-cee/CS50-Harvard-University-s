72273013d5e70c4160c495e6795f89ed7fb130bf
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-23 21:16:52 +0000

    Thu, Nov 23, 2023, 11:16 PM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports
 WHERE street = 'Humphrey Street';
+--Bakery withnesses--
+SELECT * FROM interviews
+WHERE transcript LIKE '%bakery%';
+
