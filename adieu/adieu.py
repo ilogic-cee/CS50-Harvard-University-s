6967c4c25ffcc572119c84b49e60ce2e7a0608bf
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-25 04:33:13 +0000

    Sat, Nov 25, 2023, 6:33 AM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,27 +1,15 @@
-import sys
+import inflect
 
-# Initialize an empty list to store names
-names = []
+l = []
+p = inflect.engine()
 
-# Prompt the user for names until they enter control-d
 while True:
     try:
-        name = input("Name: ").strip()
-        names.append(name)
+        a = str(input("Input: "))
+        l.append(a)
     except EOFError:
+        print()
         break
 
-# Construct the farewell message
-farewell_message = ""
-for i, name in enumerate(names):
-    if i == len(names) - 1:
-        farewell_message += "and " + name
-    elif i == len(names) - 2:
-        farewell_message += name
-    else:
-        farewell_message += name + ", "
-
-farewell_message = "Adieu, adieu, to " + farewell_message
-
-# Print the farewell message
-print(farewell_message)
+result = p.join(l)
+print("Adieu, adieu, to",result)
