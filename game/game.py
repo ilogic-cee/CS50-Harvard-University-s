07fad3b50affd7647ab8767fc90465fc6e3c7d0d
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-25 06:34:32 +0000

    Sat, Nov 25, 2023, 8:34 AM +02:00

--- game/game.py
+++ game/game.py
@@ -1,33 +1,4 @@
-import random
+from random import choice
 
-while True:
-
-try:
-
-n = int(input("Level: "))
-
-rnum = random.randint(1, n)
-
-guess = int(input("Guess: "))
-
-if guess < rnum:
-
-print("Too small!")
-
-continue
-
-elif guess > rnum:
-
-print("Too Large!")
-
-continue
-
-else:
-
-print("Just right!")
-
-except ValueError:
-
-continue
-
-break
+def main()
+    number = choice((list(range(1, get_level()+1))))
