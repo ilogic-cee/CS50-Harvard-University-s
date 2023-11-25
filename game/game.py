f8f9e49d77896f583cde24a2248bc03c7d17e1d2
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-25 06:36:33 +0000

    Sat, Nov 25, 2023, 8:36 AM +02:00

--- game/game.py
+++ game/game.py
@@ -4,4 +4,7 @@ def main()
     number = choice((list(range(1, get_level()+1))))
     while True:
         try:
-            
+            guess = int(input("Guess: "))
+            if guess > 0:
+                if guess < number:
+                    print("")
