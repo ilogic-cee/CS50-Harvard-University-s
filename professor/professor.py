b43f53636185b3ae0d50dc487f005c9f2acb7123
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-25 06:54:38 +0000

    Sat, Nov 25, 2023, 8:54 AM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,57 +1,48 @@
 import random
 
 
+def main():
+    level = get_level()
+    generate_integer(level)
+
 def get_level():
     while True:
         try:
             level = int(input("Level: "))
+            if level in [1,2,3]:
+                return level
         except ValueError:
-            print("Please enter a valid level (1, 2, or 3).")
-            continue
-
-        if level not in [1, 2, 3]:
-            print("Please enter a valid level (1, 2, or 3).")
-            continue
-
-        return level
-
+            pass
 
 def generate_integer(level):
-    if level not in [1, 2, 3]:
-        raise ValueError("Invalid level")
-
-    return random.randint(0, 10 ** level - 1)
-
-
-def main():
-    level = get_level()
-    correct_answers = 0
-
-    for _ in range(10):
-        num1 = generate_integer(level)
-        num2 = generate_integer(level)
-
-        for _ in range(3):
-            answer = input(f"{num1} + {num2} = ")
-
+    counter = 3
+    score = 0
+    for i in range(10):
+        if level == 1:
+            a = random.randint(0,9)
+            b = random.randint(0,9)
+        elif level == 2:
+            a = random.randint(10,99)
+            b = random.randint(10,99)
+        elif level == 3:
+            a = random.randint(100,999)
+            b = random.randint(100,999)
+        while True:
             try:
-                answer = int(answer)
+                answer = input(f"{a} + {b} = ")
+                if int(answer) == (a+b):
+                    score += 1
+                    break
+                elif answer != (a+b):
+                    counter -= 1
+                    print("EEE")
+                if counter <= 0:
+                    print(f"{a} + {b} = {a+b}")
+                    break
             except ValueError:
-                print("EEE")
-                continue
-
-            if answer == num1 + num2:
-                print("Correct!")
-                correct_answers += 1
-                break
-            else:
-                print("EEE")
-
-        if answer != num1 + num2:
-            print(f"{num1} + {num2} = {num1 + num2}")
-
-    print(f"Score: {correct_answers} / 10")
-
+                pass
+    else:
+        print(f"Score: {score}")
 
 if __name__ == "__main__":
     main()
