b6ae83a1348f60898d7d916f8a81c4ecaab71eb1
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-27 13:37:31 +0000

    Mon, Nov 27, 2023, 3:37 PM +02:00

--- Search/app.js
+++ Search/app.js
@@ -1,42 +1,43 @@
 // app.js
 const App = () => {
     return (
-      <div>
-        <nav className="navbar fixed-top navbar-expand-sm navbar-light bg-light">
-          {/* ... your existing navbar code ... */}
-        </nav>
+      React.createElement('div', null,
+        React.createElement('nav', { className: 'navbar fixed-top navbar-expand-sm navbar-light bg-light' },
+          // ... your existing navbar code ...
+        ),
 
-        <div className="container d-flex align-items-center vh-100">
-          <div className="container mt-5">
-            <div className="d-flex p-2 justify-content-center">
-              <img src="CS50ogle.png" width="400" alt="CS50ogle Logo" />
-            </div>
-            <div className="d-flex justify-content-center">
-              <div className="col-md-6 justify-content-center">
-                <div className="form">
-                  <form action="https://google.com/search" autoComplete="off" id="searchbar">
-                    <i className="fa fa-search"></i>
-                    <input type="text" name="q" id="searchterm" className="form-control form-input" placeholder="Search" />
-                    <span className="left-pan"><i className="fa fa-microphone"></i></span>
-                  </form>
-                </div>
+        React.createElement('div', { className: 'container d-flex align-items-center vh-100' },
+          React.createElement('div', { className: 'container mt-5' },
+            React.createElement('div', { className: 'd-flex p-2 justify-content-center' },
+              React.createElement('img', { src: 'CS50ogle.png', width: '400', alt: 'CS50ogle Logo' })
+            ),
 
-                <div className="d-flex justify-content-center my-4">
-                  <button type="submit" form="searchbar" formAction="https://google.com/search" className="btn btn-secondary mx-3">CS50ogle Search</button>
-                  <button onClick={myFunction} type="submit" form="searchbar" formAction="https://duckduckgo.com/" className="btn btn-secondary mx-3">I'm Feeling Lucky</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+            React.createElement('div', { className: 'd-flex justify-content-center' },
+              React.createElement('div', { className: 'col-md-6 justify-content-center' },
+                React.createElement('div', { className: 'form' },
+                  React.createElement('form', { action: 'https://google.com/search', autoComplete: 'off', id: 'searchbar' },
+                    React.createElement('i', { className: 'fa fa-search' }),
+                    React.createElement('input', { type: 'text', name: 'q', id: 'searchterm', className: 'form-control form-input', placeholder: 'Search' }),
+                    React.createElement('span', { className: 'left-pan' }, React.createElement('i', { className: 'fa fa-microphone' }))
+                  )
+                ),
+
+                React.createElement('div', { className: 'd-flex justify-content-center my-4' },
+                  React.createElement('button', { type: 'submit', form: 'searchbar', formAction: 'https://google.com/search', className: 'btn btn-secondary mx-3' }, 'CS50ogle Search'),
+                  React.createElement('button', { onClick: myFunction, type: 'submit', form: 'searchbar', formAction: 'https://duckduckgo.com/', className: 'btn btn-secondary mx-3' }, "I'm Feeling Lucky")
+                )
+              )
+            )
+          )
+        )
+      )
     );
   };
 
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(React.createElement(App), document.getElementById('root'));
 
   function myFunction() {
-    var msgElement = document.getElementById("searchterm");
+    var msgElement = document.getElementById('searchterm');
     msgElement.value = '\\' + msgElement.value;
     // alert();
     return true;
