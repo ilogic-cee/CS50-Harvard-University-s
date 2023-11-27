5f6068df5c2083064e545f8aa65017637d7a09b5
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-27 15:12:14 +0000

    Mon, Nov 27, 2023, 5:12 PM +02:00

--- Search/app.js
+++ Search/app.js
@@ -47,25 +47,16 @@ const App = () => {
                 React.createElement('button', { onClick: myFunction, type: 'submit', form: 'searchbar', formAction: 'https://duckduckgo.com/', className: 'btn btn-secondary mx-3' }, "I'm Feeling Lucky")
               )
             )
-          ),
-
-          // Additional container for Google Image Search
-          React.createElement('div', { className: 'd-flex justify-content-center mt-4' },
-            React.createElement('form', { action: 'https://www.google.com/search', autoComplete: 'off', id: 'imageSearchBar' },
-              React.createElement('i', { className: 'fa fa-search' }),
-              const App = () => {
-                return (
-                  // ... (your existing code)
-
-                  React.createElement('div', { className: 'd-flex justify-content-center my-4' },
-                    React.createElement('button', { type: 'submit', form: 'searchbar', formAction: 'https://google.com/search', className: 'btn btn-secondary mx-3 btn-google' }, 'CS50ogle Search'),
-                    React.createElement('button', { onClick: myFunction, type: 'submit', form: 'searchbar', formAction: 'https://duckduckgo.com/', className: 'btn btn-secondary mx-3 btn-google', id: 'feeling-lucky-btn' }, "I'm Feeling Lucky")
-                  )
-                );
-              };
-
-            )
           )
+        )
+      ),
+
+      // Additional container for Google Image Search
+      React.createElement('div', { className: 'd-flex justify-content-center mt-4' },
+        React.createElement('form', { action: 'https://www.google.com/search', autoComplete: 'off', id: 'imageSearchBar' },
+          React.createElement('i', { className: 'fa fa-search' }),
+          React.createElement('input', { type: 'text', name: 'q', id: 'imageSearchTerm', className: 'form-control form-input', placeholder: 'Image Search' }),
+          React.createElement('button', { type: 'submit', form: 'imageSearchBar', formAction: 'https://www.google.com/search', className: 'btn btn-secondary mx-3 btn-google' }, 'Google Image Search')
         )
       )
     )
