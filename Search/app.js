1c377fda332cd4eeede1a675e06e924873cc10bd
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-11-29 01:22:07 +0000

    Wed, Nov 29, 2023, 3:22 AM +02:00

--- Search/app.js
+++ Search/app.js
@@ -1,83 +1,118 @@
 const App = () => {
   return (
-    React.createElement('div', null,
-      React.createElement('nav', { className: 'navbar fixed-top navbar-expand-sm navbar-light bg-light' },
-        React.createElement('div', { className: 'container-fluid' },
-          React.createElement('a', { className: 'navbar-brand', href: 'index.html' }, ''),
-          React.createElement('button', { className: 'navbar-toggler', type: 'button', 'data-bs-toggle': 'collapse', 'data-bs-target': '#navbarNav', 'aria-controls': 'navbarNav', 'aria-expanded': 'false', 'aria-label': 'Toggle navigation' },
-            React.createElement('span', { className: 'navbar-toggler-icon' })
-          ),
-          React.createElement('div', { className: 'collapse navbar-collapse', id: 'navbarNav' },
-            React.createElement('ul', { className: 'navbar-nav ms-auto' },
-              React.createElement('li', { className: 'nav-item ms-auto' },
-                React.createElement('a', { className: 'nav-link', href: 'index.html' }, 'Search')
-              ),
-              React.createElement('li', { className: 'nav-item ms-auto' },
-                React.createElement('a', { className: 'nav-link', href: 'images.html' }, 'Image Search')
-              ),
-              React.createElement('li', { className: 'nav-item ms-auto' },
-                React.createElement('a', { className: 'nav-link', href: 'advanced.html' }, 'Advanced Search')
-              ),
-              React.createElement('li', { className: 'nav-item ms-auto' },
-                React.createElement('a', { className: 'nav-link', href: 'https://www.linkedin.com/in/junior-motsoko-970a311b4' }, 'About')
-              )
-            )
-          )
-        )
-      ),
+    <div>
+      <nav className="navbar fixed-top navbar-expand-sm navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="index.html"></a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item ms-auto">
+                <a className="nav-link" href="index.html">
+                  Search
+                </a>
+              </li>
+              <li className="nav-item ms-auto">
+                <a className="nav-link" href="images.html">
+                  Image Search
+                </a>
+              </li>
+              <li className="nav-item ms-auto">
+                <a className="nav-link" href="advanced.html">
+                  Advanced Search
+                </a>
+              </li>
+              <li className="nav-item ms-auto">
+                <a
+                  className="nav-link"
+                  href="https://www.linkedin.com/in/junior-motsoko-970a311b4"
+                >
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-      React.createElement('div', { className: 'container d-flex align-items-center vh-100' },
-        React.createElement('div', { className: 'container mt-5' },
-          React.createElement('div', { className: 'd-flex p-2 justify-content-center' },
-            React.createElement('img', { src: 'gooogle.png', width: '200', alt: 'Googlephoto' })
-          ),
+      <div className="container d-flex align-items-center vh-100">
+        <div className="container mt-5">
+          <div className="d-flex p-2 justify-content-center">
+            <img src="gooogle.png" width="200" alt="Googlephoto" />
+          </div>
 
-          React.createElement('div', { className: 'd-flex justify-content-center' },
-            React.createElement('div', { className: 'col-md-6 justify-content-center' },
-              React.createElement('div', { className: 'form' },
-                React.createElement('form', { action: 'https://google.com/search', autoComplete: 'off', id: 'searchbar' },
-                  React.createElement('i', { className: 'fa fa-search' }),
-                  React.createElement('input', { type: 'text', name: 'q', id: 'searchterm', className: 'form-control form-input', placeholder: 'Search' }),
-                  React.createElement('span', { className: 'left-pan' }, React.createElement('i', { className: 'fa fa-microphone' }))
-                )
-              ),
+          <div className="d-flex justify-content-center">
+            <div className="col-md-6 justify-content-center">
+              <div className="form">
+                <form
+                  action="https://google.com/search"
+                  autoComplete="off"
+                  id="searchbar"
+                >
+                  <i className="fa fa-search"></i>
+                  <input
+                    type="text"
+                    name="q"
+                    id="searchterm"
+                    className="form-control form-input"
+                    placeholder="Search"
+                  />
+                  <span className="left-pan">
+                    <i className="fa fa-microphone"></i>
+                  </span>
+                </form>
+              </div>
 
-              React.createElement('div', { className: 'd-flex justify-content-center my-4' },
-                React.createElement('button', { type: 'submit', form: 'searchbar', formAction: 'https://google.com/search', className: 'btn btn-secondary mx-3' }, 'CS50ogle Search'),
-                React.createElement('button', { onClick: myFunction, type: 'submit', form: 'searchbar', formAction: 'https://duckduckgo.com/', className: 'btn btn-secondary mx-3' }, "I'm Feeling Lucky")
-              )
-            )
-          )
-        )
-      ),
+              <div className="d-flex justify-content-center my-4">
+                <button
+                  type="submit"
+                  form="searchbar"
+                  formAction="https://google.com/search"
+                  className="btn btn-secondary mx-3"
+                >
+                  CS50ogle Search
+                </button>
+                <button
+                  onClick={myFunction}
+                  type="submit"
+                  form="searchbar"
+                  formAction="https://duckduckgo.com/"
+                  className="btn btn-secondary mx-3"
+                >
+                  I'm Feeling Lucky
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-
-      // Additional container for Google Image Search
-      React.createElement('div', { className: 'd-flex justify-content-center mt-4' },
-        React.createElement('form', { action: 'https://www.google.com/search', autoComplete: 'off', id: 'imageSearchBar' },
-
-
-
-        )
-      )
-    )
+      {/* Additional container for Google Image Search */}
+      <div className="d-flex justify-content-center mt-4">
+        <form
+          action="https://www.google.com/search"
+          autoComplete="off"
+          id="imageSearchBar"
+        ></form>
+      </div>
+    </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 function myFunction() {
   var msgElement = document.getElementById('searchterm');
   msgElement.value = '\\' + msgElement.value;
   return true;
 }
-
-/* Add this to your existing CSS */
-
-/* Remove the border from the top of the navbar */
-.navbar {
-  border-top: none !important; /* Use !important to override Bootstrap styles */
-}
-
-
-
