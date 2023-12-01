66322401648e9ad25ab5f3937a3319c0bab560ab
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-01 22:42:41 +0000

    Sat, Dec 2, 2023, 12:42 AM +02:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,21 +5,23 @@ from . import util
 
 def convert_md_to_html(title):
     content = util.get_entry(title)
- markdowner = markdown.Markdown()
-    if content == None:
-       return None
+    markdowner = markdown.Markdown()
+    if content is None:
+        return None
     else:
-     return markdowner.convert(content)
+        return markdowner.convert(content)
 
 def index(request):
-   return render(request, "encyclopedia/index.html", {
+    return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, title):
     html_content = convert_md_to_html(title)
-    if html_content == None:
+    if html_content is None:
         return render(request, "encyclopedia/error.html")
     else:
-        return render(request, "encylopedia/entry.html")
-
+        return render(request, "encylopedia/entry.html", {
+            "title": title,
+            "html_content": html_content
+        })
