467b55fe9b2c0c27a8fc3edb99bebe0da54003cf
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-01 23:51:38 +0000

    Sat, Dec 2, 2023, 1:51 AM +02:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("wiki/css", views.css_view, name="css_view"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("", views.index, name="index"),
 ]
