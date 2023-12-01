c3e507e32af71252bc14659b0d4f94bf65270511
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-01 22:58:12 +0000

    Sat, Dec 2, 2023, 12:58 AM +02:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -18,7 +18,7 @@ def index(request):
 
 def entry(request, title):
     html_content = convert_md_to_html(title)
-    if html_content is None:
+    if html_content == None:
         return render(request, "encyclopedia/error.html")
     else:
         return render(request, "encyclopedia/entry.html", {
