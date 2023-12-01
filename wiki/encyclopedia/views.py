47723209715bdcced574910f547377e92ae31602
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-01 16:18:59 +0000

    Fri, Dec 1, 2023, 6:18 PM +02:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,6 +4,9 @@ from . import util
 
 
 def index(request):
+    entries = util.list_entries()
+    css_file = util.get_entry("CSS")
+    coffee = util.get_entry("coffee")
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
