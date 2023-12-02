a8ba5832978ddbaf11ae84bb83bd085f2e5a59d8
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-02 01:52:55 +0000

    Sat, Dec 2, 2023, 3:52 AM +02:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('wiki/css', views.css_view, name='css_view'),  # Place this before the entry pattern
     path('wiki/<str:title>', views.entry, name='entry'),
+    path("wiki/git", views.git_view, name="git_view"),
 ]
