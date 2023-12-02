1b629592f3f3d81f03aa040f84f9a759f050be1e
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-02 01:36:35 +0000

    Sat, Dec 2, 2023, 3:36 AM +02:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -28,37 +28,6 @@ def entry(request, title):
             "content": html_content
         })
 
-
-from django.shortcuts import render
-import markdown
-
-from . import util
-
-def convert_md_to_html(title):
-    content = util.get_entry(title)
-    markdowner = markdown.Markdown()
-    if content is None:
-        return None
-    else:
-        return markdowner.convert(content)
-
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
-
-def entry(request, title):
-    html_content = convert_md_to_html(title)
-    if html_content is None:
-        return render(request, "encyclopedia/error.html",{
-            "message": "This entry does not exist"
-        })
-    else:
-        return render(request, "encyclopedia/entry.html", {
-            "title": title,
-            "content": html_content
-        })
-
 def css_view(request):
     # Your logic for the css_view goes here
     return render(request, "encyclopedia/css_view.html")  # You should create this HTML file
