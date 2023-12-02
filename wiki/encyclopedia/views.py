12e81535a81d2b0237422120dfed26aac8d61bf3
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-02 01:29:20 +0000

    Sat, Dec 2, 2023, 3:29 AM +02:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -28,3 +28,42 @@ def entry(request, title):
             "content": html_content
         })
 
+It seems like the `css_view` function is missing in your `views.py` file. You need to add the `css_view` function to resolve the `AttributeError` you are encountering.
+
+Here's the modified `views.py` with the addition of `css_view`:
+
+```python
+from django.shortcuts import render
+import markdown
+
+from . import util
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content is None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    html_content = convert_md_to_html(title)
+    if html_content is None:
+        return render(request, "encyclopedia/error.html",{
+            "message": "This entry does not exist"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
+
+def css_view(request):
+    # Your logic for the css_view goes here
+    return render(request, "encyclopedia/css_view.html")  # You should create this HTML file
+
