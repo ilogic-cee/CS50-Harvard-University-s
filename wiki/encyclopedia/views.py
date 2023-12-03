c41fa6cea9565087df17fdcde2f4a6d1298aab7d
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-03 17:53:31 +0000

    Sun, Dec 3, 2023, 7:53 PM +02:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import markdown
 from .forms import CreateEntryForm
 from .util import save_entry, get_entry, list_entries, convert_md_to_html
