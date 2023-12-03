1e02fe2b5b735472ab22f4f1323ac2715956c20c
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-03 22:54:50 +0000

    Mon, Dec 4, 2023, 12:54 AM +02:00

--- wiki/encyclopedia/forms.py
+++ wiki/encyclopedia/forms.py
@@ -4,3 +4,8 @@ from .models import Entry
 class CreateEntryForm(forms.Form):
     title = forms.CharField(label='Title', max_length=100)
     content = forms.CharField(widget=forms.Textarea, label='Content')
+
+class EditEntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['title', 'content']
