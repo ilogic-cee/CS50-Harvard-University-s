e2886665bb266153ef0bc5c507076e9485d261a0
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-04 00:49:52 +0000

    Mon, Dec 4, 2023, 2:49 AM +02:00

--- wiki/encyclopedia/models.py
+++ wiki/encyclopedia/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
-# Create your models here.
-
 class Entry(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
 
     def __str__(self):
         return self.title
-
-
-    class Meta:
-        # Add this line to set a custom primary key
-        default_auto_field = 'django.db.models.AutoField'
