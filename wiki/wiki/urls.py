5b5bc7b038f91b7bc297efe8a96163fea846c2a7
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-05 15:33:01 +0000

    Tue, Dec 5, 2023, 5:33 PM +02:00

--- wiki/wiki/urls.py
+++ wiki/wiki/urls.py
@@ -1,22 +1,19 @@
-"""wiki URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("encyclopedia.urls"))
+    path('', views.index, name='index'),
+    path('wiki/css', views.css_view, name='css_view'),
+    path('wiki/django', views.django_view, name='django_view'),
+    path('wiki/git', views.git_view, name='git_view'),
+    path('wiki/html', views.html_view, name='html_view'),
+    path('wiki/python', views.python_view, name='python_view'),
+    path('random/', views.custom_random_page, name='random_page'),  # Placed before the generic entry pattern
+    path('wiki/<str:title>', views.entry, name='entry'),
+    path('wiki/django', views.django_view, name='django_view'),
+    path('search/', views.search_form, name='search'),  # Update to search_form
+    path('entry/<str:title>/', views.entry, name='entry'),
+    path('new/', views.new_page, name='new_page'),  # Creating a new URL pattern for a new page
+    path('edit/<str:entry_title>/', views.edit_entry, name='edit_entry'),
+    path('save_edit', views.save_edit, name="save_edit"),
 ]
