85643b6af28ce1233f662d73a53cb394444c314e
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-05 06:25:31 +0000

    Tue, Dec 5, 2023, 8:25 AM +02:00

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -219,8 +219,15 @@ def edit_entry(request, entry_title):
 
 
 def save_edit(request):
-    
-    return
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        util.save_entry(title, content)
+        html_content = convert_md_to_html(title)
+    return render(request, "encyclopedia/entry.html",{
+        "title": title,
+        "content": html_content
+    })
 
 # views.py
 class MyView(View):
