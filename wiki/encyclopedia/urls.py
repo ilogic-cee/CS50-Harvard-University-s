6e3016d77c3abfe1a0b76e19d69471bcd062de6f
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-05 16:47:54 +0000

    Tue, Dec 5, 2023, 6:47 PM +02:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,8 +3,6 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 
 urlpatterns = [
     path('', views.index, name='index'),
