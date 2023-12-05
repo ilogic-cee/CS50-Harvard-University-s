85aaa624f506882076a637f5b5ae0377f1e60f03
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-05 17:33:34 +0000

    Tue, Dec 5, 2023, 7:33 PM +02:00

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .views import custom_random_page
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('wiki/git', views.git_view, name='git_view'),
     path('wiki/html', views.html_view, name='html_view'),
     path('wiki/python', views.python_view, name='python_view'),
-    path('random/', views.custom_random_page, name='random_page'),  # Placed before the generic entry pattern
+    path('random/', custom_random_page, name='random_page'), # Placed before the generic entry pattern
     path('wiki/<str:title>', views.entry, name='entry'),
     path('wiki/django', views.django_view, name='django_view'),
     path('search/', views.search_form, name='search'),  # Update to search_form
