3698d1227bc5949532121806fb858d407ed74763
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-05 16:38:08 +0000

    Tue, Dec 5, 2023, 6:38 PM +02:00

---
+++ wiki/wiki/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('encyclopedia.urls')),  # Include the URLs from your app
+    # Add other paths as needed
+]
