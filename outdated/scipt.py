ea219784007dd38ba7bf738623b713d0ec64fd34
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-13 18:32:05 +0000

    Wed, Dec 13, 2023, 8:32 PM +02:00

--- outdated/scipt.py
+++ outdated/scipt.py
@@ -21,7 +21,7 @@ import os
 import sys
 
 
-def main():
+def main():si
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki.settings')
     try:
         from django.core.management import execute_from_command_line
