47bb9377beab993020a2c2dd2eb419a4f4a5cc5a
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-13 18:36:28 +0000

    Wed, Dec 13, 2023, 8:36 PM +02:00

--- outdated/scipt.py
+++ outdated/scipt.py
@@ -1,5 +1,4 @@
-hello, world welcome to my YouTube channel, I am IlogicCee jr ,
-and this is my submission for cs50 Web Programming with python and JavaScript project 0,
+hello, world welcome to my YouTube channel, I am IlogicCee jr , and this is my submission for cs50 Web Programming with python and JavaScript project 0,
 
 the project is called search, where i had to design a front end for google search, google image search and google advanced search
 
@@ -9,6 +8,24 @@ First off, we've got our Google Search page, Check out the upper-right corner, w
 [Google Search Functionality]
 
 and here we are, on the search results page for our query Our. search bar is centered with rounded corners, close to Google's.
+
+NOW let's type a query “rugby sa” and hit "Google Search.” button;
+
+02:01 - 02:30]
+[Google Advanced Search Page]
+Speaker (On Screen): Now, let's explore the Advanced Search page. Back to Google Search from here. Timestamp: [02:05]. Here, we've got four input fields—stacked just like Google's.
+
+[02:31 - 03:00]
+[Advanced Search Functionality]
+Speaker (Screen Recording): Inputting our search criteria. Timestamp: [02:35]. And when we hit the blue "Advanced Search" button, it takes us to the search results page. Timestamp: [02:40].
+
+[03:01 - 03:30]
+[I'm Feeling Lucky Button]
+Speaker (On Screen): Let's not forget the "I'm Feeling Lucky" button on the main Google Search page. Timestamp: [03:05]. Clicking it will take us directly to the first result—bypassing the normal results page. Timestamp: [03:10].
+
+[03:31 - 04:00]
+[Conclusion]
+Speaker (On Screen): There you have it, folks! The Search Google Clone project in action. We've covered links, search functionality, advanced search, and the "I'm Feeling Lucky" feature. If you have any questions, drop them in the comments. Thanks for watching!
 
 NOW let's type a query “rugby sa” and hit "Google Search.” button;
 [Closing Shot]
