4d9fa26d3e9e5e871dc369a2520bffc7918bed1d
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-13 09:34:14 +0000

    Wed, Dec 13, 2023, 11:34 AM +02:00

--- outdated/scipt.py
+++ outdated/scipt.py
@@ -1,5 +1,29 @@
 
+run (On Screen): Speaking of the description, down there, you'll find timestamps. Yeah, I got you covered, so you can skip around if you want to revisit any specific part of the demo.
 
+[03:31 - 04:00]
+[Enhanced Functionality]
+Arun (On Screen): Now, let's talk about the enhanced functionality. The Search Google Clone brings not just regular search, but also Google Image Search and Google Advanced Search. Check out these links in the upper-right corner to switch between them seamlessly.
+
+[04:01 - 04:30]
+[Google Image Search]
+Arun (Screen Recording): Now, let's explore the Image Search page. You can type in a query, hit search, and voila! You'll be taken to the Google Image search results for that page.
+
+[04:31 - 05:00]
+[Google Advanced Search]
+Arun (Screen Recording): And here's the Advanced Search page. Just like Google's own, you can input specific search criteria. When you hit the "Advanced Search" button, you're taken to the search results page for your query.
+
+[05:01 - 05:30]
+[Additional Features]
+Arun (On Screen): Oh, and check this out! On the main Google Search page, we've got an "I'm Feeling Lucky" button. Clicking this will take you directly to the first search result for your query, bypassing the normal results page. Now, that's a time-saver!
+
+[05:31 - 06:00]
+[Closing Thoughts]
+Arun (On Screen): And there you have it, guys. That was a quick tour of the enhanced Search Google Clone project. If you've got questions or thoughts, drop them in the comments below. Don't forget to check out the timestamps for easy navigation. Until next time, I'm Arun from Mr. Who's the Boss, signing off.
+
+[Closing Shot]
+[Outro Music Playing]
+[Wide shot of Arun reaching to turn off the camera]
 
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
