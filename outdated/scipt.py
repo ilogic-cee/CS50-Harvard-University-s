8ac3c319f1a4b65494be5ca20f44bc8385c1deba
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-13 09:37:07 +0000

    Wed, Dec 13, 2023, 11:37 AM +02:00

--- outdated/scipt.py
+++ outdated/scipt.py
@@ -36,7 +36,7 @@ def main():
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
+        raise ImportError(8
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
