c9537210508743f31adbf24ea5415a842af96edc
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-14 09:03:57 +0000

    Thu, Dec 14, 2023, 11:03 AM +02:00

---
+++ commerce/auctions/forms.py
@@ -0,0 +1,8 @@
+# auctions/forms.py
+from django import forms
+from .models import Listing
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['title', 'description', 'starting_bid', 'image_url', 'category']
