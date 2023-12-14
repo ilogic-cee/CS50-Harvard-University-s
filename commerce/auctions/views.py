b3627e44458b5103ad56da2b3547f104bfb42232
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-14 14:21:26 +0000

    Thu, Dec 14, 2023, 4:21 PM +02:00

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import ListingForm
 from django.contrib.auth.decorators import login_required
