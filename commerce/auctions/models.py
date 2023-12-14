fb0bef2f59878fb6d462bc5d2d4f5478e1fab779
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-14 06:36:53 +0000

    Thu, Dec 14, 2023, 8:36 AM +02:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    titile =models.CharField(max_length=300)
+    description - models.CharField(max_length=300)
+    imageUrl = models.CharField(max_length=1000)
+    price models.FloatField()
+    isActive = models.BooleanField(default=time)
+    owner models.ForeighKey(User, on_delete)
