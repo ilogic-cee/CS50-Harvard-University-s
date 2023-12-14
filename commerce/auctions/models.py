25f5079951637ecbc92e60f2d5460248d5b7332b
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-14 06:40:08 +0000

    Thu, Dec 14, 2023, 8:40 AM +02:00

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -7,9 +7,10 @@ class User(AbstractUser):
 
 
 class Listing(models.Model):
-    titile =models.CharField(max_length=300)
-    description - models.CharField(max_length=300)
+    title =models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
     imageUrl = models.CharField(max_length=1000)
-    price models.FloatField()
-    isActive = models.BooleanField(default=time)
-    owner models.ForeighKey(User, on_delete)
+    price = models.FloatField()
+    isActive = models.BooleanField(default=True)
+    owner models.ForeighKey(User, on_delete=models.CASCADE, blank=true, null=true, related_name="user")
+    category =  
