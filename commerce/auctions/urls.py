5cf2e16ac3d7c25caa7e64448bdc405b917ddf29
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-15 10:00:46 +0000

    Fri, Dec 15, 2023, 12:00 PM +02:00

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-     path('listing/', views.listing, name='listing'),
+    path('listing/', views.listing, name='listing'),
     path("create", views.createListing, name="create")
 
 
