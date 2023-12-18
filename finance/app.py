55b75798d98acd8c13bc612e3d83987fcde7e0d3
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-18 19:50:41 +0000

    Mon, Dec 18, 2023, 9:50 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -37,7 +37,7 @@ def after_request(response):
 def index():
     """Show portfolio of stocks"""
     stocks = db.execute("SELECT symbol, SUM(shares) as total_shares FROM transactions WHERE user_id = :user_id GROUP BY symbol HAVING total_shares > 0", user_id=session["user_id"]
-[0]["cash"]
+
 
 
     cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
