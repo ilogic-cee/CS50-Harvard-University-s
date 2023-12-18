6f5f82e1f2b0e7895c6c1a5c684b4620c5225fee
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-18 15:19:08 +0000

    Mon, Dec 18, 2023, 5:19 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -42,6 +42,44 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
+    if request.method == "GET":
+        return render_template("buy.html")
+    else:
+        symbol = request.form.get("symbol")
+        shares = int(request.form.get("shares"))
+
+        if not symbol:
+                return apology("Must Give Symbol")
+
+        stock = lookup(symbol.upper())
+
+        if stock == None:
+            return apology("Symbol Does Not Exist")
+
+        if shares < 0:
+            return apology("Share Not Allowed")
+
+        transaction_value = shares * stock["price"]
+
+        user_id = session["user_id"]
+        user_cash_db = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
+        user_cash = user_cash_db[0]["cash"]
+
+        if user_cash < transaction_value:
+            return apology("Not Enough Money")
+        uptd_cash = user_cash - transaction_value
+
+        db.execute("UPDATE users SET cash = ? WHERE id = ?", uptd_cash, user_id)
+
+        date = datetime.datetime.now()
+
+        db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date) VALUES (?, ?, ?, ?, ?)", user_id, stock["symbol"], shares, stock["price"], date)
+
+        flash("Boudht!")
+
+        return redirect("/")
+
+    """Buy shares of stock"""
     return apology("TODO")
 
 
