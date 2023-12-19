3e20f9e42311e2276a9bb129fd92ab551d3d14ef
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-19 14:15:46 +0000

    Tue, Dec 19, 2023, 4:15 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -56,8 +56,40 @@ def index():
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
-    """Buy shares of stock"""
-    return apology("TODO")
+    if request.method == "GET":
+        return render_template("buy.html")
+    else:
+        capital= db.execute("SELECT cash FROM users WHERE id=:userid",userid=session["user_id"])
+        if request.form.get("symbol") == None:
+            return apology("Symbol required")
+
+        elif request.form.get("shares") == None:
+            return apology("Invalid quantity")
+
+        elif lookup(request.form.get("symbol")) == None:
+            return apology("Invalid symbol")
+
+        elif capital[0]["cash"] < lookup(request.form.get("symbol"))["price"] * int(request.form.get("shares")):
+            return apology("Insufficient funds")
+
+        else:
+            today = date.today()
+            d = today.strftime("%d/%m/%Y")
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            response=lookup(request.form.get("symbol"))
+            amount= response["price"] * int(request.form.get("shares"))
+            response=lookup(request.form.get("symbol"))
+            trade= db.execute("INSERT INTO transactions (userid,date,time,symbol,name,price,quantity,total) VALUES (:userid,:date,:time,:symbol,:name,:price,:quantity,:total)",userid=session["user_id"],date=d,time=current_time,symbol=request.form.get("symbol"), name=response["name"], price=response["price"],quantity=request.form.get("shares"),total=amount )
+            update= db.execute("UPDATE users SET cash=:amt WHERE id=:userid ", amt=capital[0]["cash"]-amount,userid=session["user_id"])
+            check=db.execute("SELECT symbol FROM holdings WHERE symbol=:symbol AND userid=:userid",symbol=request.form.get("symbol"),userid=session["user_id"])
+            if len(check) < 1:
+                update_holdings=db.execute("INSERT INTO holdings (userid,symbol,name,quantity) VALUES (:userid,:symbol,:name,:quantity)", userid=session["user_id"], symbol=request.form.get("symbol"),name=lookup(request.form.get("symbol"))["name"],quantity=request.form.get("shares"))
+            else:
+                quant=db.execute("SELECT quantity FROM holdings WHERE userid=:userid AND symbol=:symbol ",userid=session["user_id"], symbol=request.form.get("symbol"))
+                update_holdings=db.execute("UPDATE holdings SET quantity=:q WHERE symbol=:symbol AND userid=:userid",q=int(quant[0]["quantity"])+int(request.form.get("shares")), symbol=request.form.get("symbol"),userid=session["user_id"])
+            flash('Bought!')
+            return redirect("/")
 
 
 @app.route("/history")
