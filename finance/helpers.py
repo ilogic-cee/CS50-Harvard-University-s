6e8e87b1c5c1516eefe04b05507b38d84591ce2f
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-19 13:49:19 +0000

    Tue, Dec 19, 2023, 3:49 PM +02:00

--- finance/helpers.py
+++ finance/helpers.py
@@ -24,6 +24,28 @@ def apology(message, code=400):
         return s
     return render_template("apology.html", top=code, bottom=escape(message)), code
 
+def after_request(response):
+    response.headers["Cache-Control"] = "no=cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use POSTGRES database
+db = SQL(os.getenv("DATABASE_UR"))
+
+# Make sure API key is set
+# if not os.environ.get("API_KEY"):
+#     raise RuntimeError("API_KEY not set")
+
 
 def login_required(f):
     """
