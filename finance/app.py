91c828ec587cc022b97777930424769ffef6f5b4
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-19 14:19:25 +0000

    Tue, Dec 19, 2023, 4:19 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -96,43 +96,38 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    transactions = db.execute(
-        "SELECT * FROM transactions WHERE user_id = :user_id ORDER BY timestamp DESC", user_id=session["user_id"])
+    query = db.execute("SELECT * FROM transactions WHERE userid = :userid ORDER BY id DESC", userid=session["user_id"])
+    return render_template("history.html", query=query)
 
-    return render_template("history.html", transactions=transactions)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
-    # Forget any user_id
-    session.clear()
+    session.pop("user_id", None)
 
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
 
-        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
-        # Ensure username exists and password is correct
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
-        # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
-        # Redirect user to home page
+
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
+
     else:
         return render_template("login.html")
 
