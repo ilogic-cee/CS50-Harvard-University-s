a1dc1d24dca3eed28d2e76c1d0d8ae1cb8d0aa9f
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-19 14:14:03 +0000

    Tue, Dec 19, 2023, 4:14 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -16,12 +16,14 @@ app = Flask(__name__)
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
+db = SQL(os.getenv("DATABASE_UR"))
 
 
 @app.after_request
