76df579e9f0f45ccf6ee1746427eccc7587cf1f6
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-19 09:29:58 +0000

    Tue, Dec 19, 2023, 11:29 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -40,7 +40,7 @@ def after_request(response):
 def index():
     """Show portfolio of stocks"""
     # Assuming db.execute returns a list of rows
-    stocks_data = db.execute("SELECT symbol, SUM(shares) as total_shares FROM transactions WHERE user_id = :user_id GROUP BY symbol HAVING total_shares > 0", user_id=session["user_id"])
+    stocks = db.execute("SELECT symbol, SUM(shares) as total_shares FROM transactions WHERE user_id = :user_id GROUP BY symbol HAVING total_shares > 0", user_id=session["user_id"])
 
 
     cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
