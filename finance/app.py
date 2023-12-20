b5a57b9bf5763c212f86c40b061cd4683bbe803f
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-20 10:44:55 +0000

    Wed, Dec 20, 2023, 12:44 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -128,8 +128,10 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    query=db.execute("SELECT * FROM transactions WHERE userid=:userid ORDER BY id DESC",userid=session["user_id"])
-    return render_template("history.html",query=query)
+
+    # Put history of user in a variable
+    transactions = db.execute("SELECT symbol, shares, price, time FROM stocks WHERE id = ?", session["user_id"])
+    return render_template("history.html", transactions=transactions)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -319,3 +321,38 @@ def errorhandler(e):
 # Listen for errors
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
+
+@app.route("/user", methods=["GET", "POST"])
+@login_required
+def user():
+    """Change password of user"""
+
+    # User has reached route via POST
+    if request.method == "POST":
+
+        # Prompt user for old and new password, and confirmation
+        oldPassword = db.execute("SELECT hash FROM users WHERE id = ?", session["user_id"])
+        currentPassword = request.form.get("current_password")
+        newPassword = request.form.get("new_password")
+        newConfirmation = request.form.get("new_confirmation")
+
+        # Check for user error
+        if not currentPassword or not newPassword or not newConfirmation:
+            return apology("missing fields")
+        elif not check_password_hash(oldPassword[0]["hash"], currentPassword):
+            return apology("invalid current password")
+        elif newPassword != newConfirmation:
+            return apology("passwords do not match")
+
+        # Generate new password hash
+        newPasswordHash = generate_password_hash(newPassword)
+
+        # Update password
+        db.execute("UPDATE users SET hash = ? WHERE id = ?", newPasswordHash, session["user_id"])
+
+        flash("Password Changed!")
+
+        return redirect("/user")
+    else:
+        userName = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])
+        return render_template("user.html", userName=userName[0]["username"])
