f832b94fdd846548609b13b5bdc3f47d908eb57a
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-23 16:53:51 +0000

    Sat, Dec 23, 2023, 6:53 PM +02:00

--- lines/lines.py
+++ lines/lines.py
@@ -2,26 +2,20 @@ import sys
 
 
 def main():
-    if len(sys.argv) < 2:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-    else:
-        if not sys.argv[1].endswith(".py"):
+    if len(sys.argv) != 2:
+        sys.exit("Incorrect number of command-line arguments")
 
-            sys.exit("Not a python file")
-        else:
-            print(count_lines(sys.argv[1]))
+    if not sys.argv[1].endswith(".py"):
+        sys.exit("Not a python file")
+
+    print(count_lines(sys.argv[1]))
 
 
 def count_lines(file):
     try:
-        counter = 0
         with open(file, "r") as f:
-            for line in f:
-                if not (line.lstrip().startswith("#") or line.strip() == ""):
-                    counter = counter + 1
-            return counter
+            counter = sum(1 for line in f if not line.lstrip().startswith("#") and not line.isspace())
+        return counter
     except FileNotFoundError:
         sys.exit("File does not exist")
 
