90ac2c28a869bc8d7b39b89822a0859a4882431e
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-31 18:05:39 +0000

    Sun, Dec 31, 2023, 8:05 PM +02:00

---
+++ project_py/test_project.py
@@ -0,0 +1,11 @@
+# test_project.py
+
+def test_evaluate():
+    # Setup
+    state = ...  # define a board state
+
+    # Invoke
+    result = evaluate(state)
+
+    # Assert
+    assert result == expected_value  # Replace 'expected_value' with what you expect
