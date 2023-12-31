5447ada5972ea2ea1080be23163758fdfc5d3e80
ilogic <81265920+ilogic-cee@users.noreply.github.com>
2023-12-31 18:07:33 +0000

    Sun, Dec 31, 2023, 8:07 PM +02:00

--- project_py/test_project.py
+++ project_py/test_project.py
@@ -1,11 +1,95 @@
 # test_project.py
+from project import evaluate, wins, game_over
 
-def test_evaluate():
-    # Setup
-    state = ...  # define a board state
+def test_evaluate_win():
+    """
+    Test evaluate function for a winning condition.
+    """
+    # Example board where COMP has won
+    board = [
+        [+1, +1, +1],
+        [0, -1, -1],
+        [0, 0, 0]
+    ]
+    assert evaluate(board) == +1
 
-    # Invoke
-    result = evaluate(state)
+def test_evaluate_loss():
+    """
+    Test evaluate function for a losing condition.
+    """
+    # Example board where HUMAN has won
+    board = [
+        [-1, -1, -1],
+        [0, +1, +1],
+        [0, 0, 0]
+    ]
+    assert evaluate(board) == -1
 
-    # Assert
-    assert result == expected_value  # Replace 'expected_value' with what you expect
+def test_evaluate_draw():
+    """
+    Test evaluate function for a draw condition.
+    """
+    # Example board for a draw
+    board = [
+        [+1, -1, +1],
+        [-1, -1, +1],
+        [+1, +1, -1]
+    ]
+    assert evaluate(board) == 0
+
+def test_wins():
+    """
+    Test wins function for various win conditions.
+    """
+    board = [
+        [+1, +1, +1],
+        [0, -1, -1],
+        [0, 0, 0]
+    ]
+    assert wins(board, +1)
+
+    board = [
+        [-1, 0, 0],
+        [-1, +1, +1],
+        [-1, 0, 0]
+    ]
+    assert wins(board, -1)
+
+    board = [
+        [+1, -1, 0],
+        [0, +1, -1],
+        [-1, 0, +1]
+    ]
+    assert not wins(board, +1)
+
+def test_game_over():
+    """
+    Test game_over function for various game states.
+    """
+    board = [
+        [+1, +1, +1],
+        [0, -1, -1],
+        [0, 0, 0]
+    ]
+    assert game_over(board)
+
+    board = [
+        [-1, -1, -1],
+        [0, +1, +1],
+        [0, 0, 0]
+    ]
+    assert game_over(board)
+
+    board = [
+        [+1, -1, +1],
+        [-1, -1, +1],
+        [+1, +1, -1]
+    ]
+    assert game_over(board)
+
+    board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]
+    assert not game_over(board)
